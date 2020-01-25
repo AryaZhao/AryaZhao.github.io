9291a1e38dfcf0c15da428311901c1b1122fddb2
Arya Zhao <lz2650@columbia.edu>
2020-01-24 21:24:46 -0500

    change background color

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function setup() {
 	  background(225);
 	  noFill();
 	  strokeWeight(5);
-	  stroke(50+255*second()/60);
+	  stroke(75+(255-75)*second()/60);
 	  rotateY(millis() / 1000);
 	  arc(0, 0,150, 150, -HALF_PI,  -HALF_PI+TWO_PI*hour()/24);
 	  rotateY(2*millis() / 1000);
